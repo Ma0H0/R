365f2d625384bda32401e9c33fcc32907bfe96e4
s30718 <s30718@pjwstk.edu.pl>
2025-06-11 15:57:07 +0200

    Usuwanie zbędnych plików z repository

--- Kolokwium2GrupaA/Kolokwium2GrupaA/Services/IDbService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Kolokwium2GrupaA.DTOs;
-
-namespace Kolokwium2GrupaA.Services;
-
-public interface IDbService
-{
-    Task<CustomerDTO> GetCustomerById(int CustomerId);   
-    Task<RequestAddWashingMachineDTO> AddWashingMachine(RequestAddWashingMachineDTO washingMachine);
-}
